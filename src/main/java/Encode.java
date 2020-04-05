a44cc910c7d8f5d4acb5bf6a8d8ba9f2f2f261f2
Robin Vonk <robinvonk789@gmail.com>
2020-04-05 15:52:16 +0200

    Added step 2 and 3 to GUI

--- src/main/java/Encode.java
+++ src/main/java/Encode.java
@@ -8,18 +8,17 @@ import java.awt.*;
 
 public class Encode extends JPanel {
 
-    private JButton NButton;
-    private JLabel Plab, Qlab, PQTime;
-    private JTextField NInput;
+    private JLabel Plab, Qlab, PQTime, Evalue, Mvalue;
+    private JTextField NInput, Minput;
 
-    private long N, P, Q = 0;
+    private long N, P, Q, E = 0;
 
     public Encode(){
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         JLabel title = new JLabel("Encode");
         JLabel NLabel = new JLabel("Give me the N:");
-        NInput = new JTextField();
-        NButton = new JButton("Send N");
+        NInput = new JTextField("123");
+        JButton NButton = new JButton("Step 1: Send N");
         JLabel Pis = new JLabel("P is");
         Plab = new JLabel("0");
         JLabel Qis = new JLabel("Q is");
@@ -27,12 +26,22 @@ public class Encode extends JPanel {
         JLabel PQTimeLabel = new JLabel("Amount of time busy finding p and q (Nanoseconds):");
         PQTime = new JLabel("0");
 
+        JButton EButton = new JButton("Step 2: Generate an E");
+        JLabel Elab =  new JLabel("E is: ");
+        Evalue = new JLabel("0");
+
+        Minput = new JTextField("Hello");
+        JButton MButton = new JButton("Step 3: Encrypt M");
+        JLabel MAfterEnc = new JLabel("Message after encryption is: ");
+        Mvalue = new JLabel("0");
+
+
         NInput.setMaximumSize(new Dimension(99999999, 20));
+        Minput.setMaximumSize(new Dimension(99999999, 20));
+
         NButton.addActionListener(actionEvent -> NButtonPressed());
-        NLabel.setLabelFor(NInput);
-        Pis.setLabelFor(Plab);
-        Qis.setLabelFor(Qlab);
-        PQTimeLabel.setLabelFor(PQTime);
+        EButton.addActionListener(actionEvent -> EButtonPressed());
+        MButton.addActionListener(actionEvent -> MButtonPressed());
 
         add(title);
         add(NLabel);
@@ -44,6 +53,13 @@ public class Encode extends JPanel {
         add(Qlab);
         add(PQTimeLabel);
         add(PQTime);
+        add(EButton);
+        add(Elab);
+        add(Evalue);
+        add(Minput);
+        add(MButton);
+        add(MAfterEnc);
+        add(Mvalue);
     }
 
     public void showP(long value){
@@ -56,8 +72,21 @@ public class Encode extends JPanel {
     public void showPQTime(long value){
         PQTime.setText(Long.toString(value));
     }
+
+    public void showE(long value){
+        Evalue.setText(Long.toString(value));
+    }
+
+    public void showM(long value){
+        Mvalue.setText(Long.toString(value));
+    }
     public void NButtonPressed(){
-        N = Long.parseLong(NInput.getText());
+        try {
+            N = Long.parseLong(NInput.getText());
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(this,"Invalid N provided");
+            return;
+        }
         if (N == 0){
             JOptionPane.showMessageDialog(this,"N Cannot be 0");
             return;
@@ -69,5 +98,13 @@ public class Encode extends JPanel {
         Q = rsa.getQ();
         showP(P);
         showQ(Q);
+    }
+
+    public void EButtonPressed(){
+        showE(123);
+    }
+
+    public void MButtonPressed(){
+        showM(69);
     }
 }
